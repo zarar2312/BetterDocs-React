ea2921cb5454c7ca54740bb7d78ebbe5705379c9
Christopher <christopher.00.ramos@gmail.com>
2018-12-19 21:27:57 -0800

    Added footer to profile pages

--- src/templates/author-page.js
+++ src/templates/author-page.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Layout from '../components/layout-mobile-footer'
+import Layout from '../components/layout'
 import style from '../styles/author-page.module.scss'
 import { graphql, Link } from "gatsby"
 
