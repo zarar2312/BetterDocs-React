96b63d442f856d67cc1018abb630242054797082
Christopher <christopher.00.ramos@gmail.com>
2018-12-22 23:19:43 -0800

    Updated

--- src/pages/download.js
+++ src/pages/download.js
@@ -11,19 +11,53 @@ const DownloadPage = () => (
     >
     <div className={style.header}
     >
-    BandagedBD
+    <p className={style.h1}>BandagedBD</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
-    Most Recommended (Page WIP)
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature}>Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
+    
     </div>
     </a>
     <a className={style.card} target="blank" alt="EnhancedDiscord" href="https://enhanceddiscord.com/"
     >
     <div className={style.header}
     >
-    EnhancedDiscord
+    <p className={style.h1}>EnhancedDiscord</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature}>Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
     
     </div>
     </a>
@@ -31,9 +65,26 @@ const DownloadPage = () => (
     >
     <div className={style.header}
     >
-    DiscordInjections
+    <p className={style.h1}>DiscordInjections</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature}>Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
     
     </div>
     </a>
@@ -41,9 +92,26 @@ const DownloadPage = () => (
     >
     <div className={style.header}
     >
-    BeautifulDiscord
+    <p className={style.h1}>BeautifulDiscord</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature} alt="null">Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
     
     </div>
     </a>
@@ -51,22 +119,57 @@ const DownloadPage = () => (
     >
     <div className={style.header}
     >
-    PowerCord
+    <p className={style.h1}>PowerCord</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature}>Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
     
     </div>
     </a>
-    <a className={style.card} target="blank" alt="BetterDiscord" href="https://github.com/Jiiks/BetterDiscordApp/releases"
+    <div className={style.card} target="blank" alt="BetterDiscord" href="https://github.com/Jiiks/BetterDiscordApp/releases"
     >
     <div className={style.header}
     >
-    BetterDiscord
+    <p className={style.h1}>BetterDiscord</p>
+    <svg className={style.wave} height='100' id='Layer_1' preserveAspectRatio='none' viewBox='0 0 300 100' width='300' xmlns='http://www.w3.org/2000/svg'>
+        <path className={style.waveLayer1} d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer2} d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z'
+        fill='#FFF' opacity='0.6' />
+        <path className={style.waveLayer3} d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716	H42.401L43.415,98.342z'
+        fill='#FFF' opacity='0.7' />
+        <path className={style.waveLayer4} d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z'
+        fill='#FFF' />
+    </svg>
     </div>
     <div className={style.body}>
+      <div className={style.features}>
+        <div className={style.feature}>Themes</div>
+        <div className={style.feature}>Plugins</div>
+        <div className={style.feature}>CSS Hot-Reload</div>
+      </div>
+    </div>
+    <div className={style.footer}>
     
     </div>
-    </a>
+      <div className={style.info}><p><b>Info: </b>This mod has been deprecated please use it's successor instead, BandagedBD.</p></div>
+    </div>
     </div>
   </Layout>
 )
