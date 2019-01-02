3fb6730e21474f71d0e874969e204c656919cbfc
Christopher <christopher.00.ramos@gmail.com>
2019-01-01 18:28:44 -0800

    icons test

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,39 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-image`,
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/mobile.png",
+  
+        // WebApp Manifest Configuration
+        //appName: null, // Inferred with your package.json
+        //appDescription: null,
+        //developerName: null,
+        //developerURL: null,
+        //dir: 'auto',
+        //lang: 'en-US',
+        //background: '#fff',
+        //theme_color: '#fff',
+        //display: 'standalone',
+        //orientation: 'any',
+        //start_url: '/?homescreen=1',
+        //version: '1.0',
+  
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: true
+        },
+      },
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: manifestConfig,
     },
