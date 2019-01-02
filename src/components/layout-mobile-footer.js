5602a4ec675ac3d428a1ce330b40190725f3b4cc
Christopher <christopher.00.ramos@gmail.com>
2019-01-01 19:06:46 -0800

    Update (icons worked)

--- src/components/layout-mobile-footer.js
+++ src/components/layout-mobile-footer.js
@@ -37,6 +37,7 @@ const Layout = ({ children }) => (
           <meta property="og:url" content="https://betterdocs.us" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#262626"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="manifest" href="/site.webmanifest"/>
           <meta name="msapplication-TileColor" content="#262626"/>
