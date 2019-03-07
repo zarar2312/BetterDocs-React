c67c49624f6fbcafea0d2cc25c33fa04374e4d4c
Christopher <christopher.00.ramos@gmail.com>
2019-03-06 23:00:20 -0800

    Update

--- src/components/themes/hero.js
+++ src/components/themes/hero.js
@@ -71,9 +71,13 @@ const Container = styled.div`
     height: calc(100vh - 50vh);
     background-size: cover;
     background-position: center;
+    transition: 350ms linear all;
     @media (min-width: 850px) {
         padding: 4rem 0rem 5rem 0rem;
         background-position: unset;
+    }
+    &:hover {
+        height: calc(100vh - 35vh);
     }
     &::before {
         content: "";
